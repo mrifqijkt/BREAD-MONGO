bd07fd7dcbd6a8f0b495f6b4c2f849c14bf0c98a
Rifqi <mohamadrifki66@gmail.com>
2023-06-27 23:24:07 +0700

    c22 blom selesai

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const ObjectID = require("mongodb").ObjectId;
 var router = express.Router();
 
 module.exports = function (db) {
@@ -7,14 +8,14 @@ module.exports = function (db) {
 
   router.get('/', async function (req, res, next) {
 
-    const { page = 1, id, string, integer, float, stardate, enddate, boolean, sortBy = '_id', sortMode = 'asc' } = req.query
+    const { page = 1, id, string, integer, float, startdate, enddate, boolean, sortBy = '_id', sortMode = 'asc' } = req.query
     const params = {}
 
     const sortParams = {}
     sortParams[sortBy] = sortMode == 'asc' ? 1 : -1
 
     if (id) {
-      params['id'] = id
+      params['id'] = parseInt(id)
 
     }
 
@@ -24,26 +25,26 @@ module.exports = function (db) {
     }
 
     if (integer) {
-      params['integer'] = integer
+      params['integer'] = parseInt(integer)
 
     }
 
     if (float) {
-      params['float'] = float
+      params['float'] = parseFloat(float)
 
     }
 
-    if (stardate && enddate) {
-      params['date'] = { $gte: new Date(stardate), $lte: new Date(enddate) }
+    if (startdate && enddate) {
+      params['date'] = { $gte: new Date(startdate), $lte: new Date(enddate) }
 
-    } else if (stardate) {
-      params['date'] = { $gte: new Date(stardate) }
+    } else if (startdate) {
+      params['date'] = { $gte: new Date(startdate) }
     } else if (enddate) {
       params['date'] = { $lte: new Date(enddate) }
     }
 
     if (boolean) {
-      params['boolean'] = boolean
+      params['boolean'] = boolean === 'true'
 
     }
 
@@ -56,12 +57,24 @@ module.exports = function (db) {
     const pages = Math.ceil(total / limit);
 
     const users = await collection.find(params).sort(sortParams).limit(limit).skip(offset).toArray();
-    res.json({ users, page: parseInt(page), pages })
+    res.json({ users, page: parseInt(page), pages, offset })
   });
 
   router.post('/', async function (req, res, next) {
+    console.log(req.body)
     const user = await collection.insertOne({ id: parseInt(req.body.id), string: req.body.string, integer: parseInt(req.body.integer), float: parseFloat(req.body.float), date: new Date(req.body.date), boolean: JSON.parse(req.body.boolean) });
     res.json(user)
+  });
+
+  router.delete("/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const result = await collection.deleteOne({ _id: new ObjectID(id) });
+      res.json(result);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Error deleting data" });
+    }
   });
 
   return router;
